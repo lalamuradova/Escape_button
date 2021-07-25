c5a3af577c890e7b733ad6fd5f28ee886555e4ea
Lala Muradova <Lalamuradova2017@gmail.com>
2021-07-26 01:23:50 +0400

    Add project files.

---
+++ Escape_button/Form1.Designer.cs
@@ -0,0 +1,69 @@
+﻿
+namespace Escape_button
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.catchmebtn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // catchmebtn
+            // 
+            this.catchmebtn.AutoSize = true;
+            this.catchmebtn.BackColor = System.Drawing.Color.Teal;
+            this.catchmebtn.Font = new System.Drawing.Font("Segoe Script", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.catchmebtn.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.catchmebtn.Location = new System.Drawing.Point(463, 151);
+            this.catchmebtn.Name = "catchmebtn";
+            this.catchmebtn.Size = new System.Drawing.Size(190, 63);
+            this.catchmebtn.TabIndex = 0;
+            this.catchmebtn.Text = "Catch me";
+            this.catchmebtn.UseVisualStyleBackColor = false;
+            this.catchmebtn.MouseEnter += new System.EventHandler(this.catchmebtn_MouseEnter);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1013, 698);
+            this.Controls.Add(this.catchmebtn);
+            this.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button catchmebtn;
+    }
+}
+
---
+++ Escape_button/Form1.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Escape_button
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void catchmebtn_MouseEnter(object sender, EventArgs e)
+        {
+            Random r = new Random();
+            catchmebtn.BackColor = Color.FromArgb(r.Next(0, 256),
+                 r.Next(0, 256), r.Next(0, 256));
+
+            this.BackColor = catchmebtn.BackColor;
+
+            if (sender is Button bt)
+            {
+                bt.Dispose();
+
+                Button button = new Button();
+                button.Font = new Font("Segoe Script", 16, FontStyle.Bold);
+                button.Text = "Catch me";
+                button.AutoSize = true;
+                button.MouseEnter += Button_MouseEnter;
+
+                Random random = new Random();
+                int randomX = random.Next(this.Size.Width - 100);
+                int randomY = random.Next(this.Size.Height - 100);
+                button.Location = new Point(randomX, randomY);
+
+                this.Controls.Add(button);
+
+
+            }          
+
+            
+        }
+
+       
+        private void Button_MouseEnter(object sender, EventArgs e)
+        {
+            Random r = new Random();
+            catchmebtn.BackColor = Color.FromArgb(r.Next(0, 256),
+                 r.Next(0, 256), r.Next(0, 256));
+
+            this.BackColor = catchmebtn.BackColor;
+
+            if (sender is Button bt)
+            {
+                bt.Dispose();
+
+                Button button = new Button();
+                button.Font = new Font("Segoe Script", 16, FontStyle.Bold);
+                button.Text = "Catch me";
+                button.AutoSize = true;
+                button.MouseEnter += Button_MouseEnter;
+
+                Random random = new Random();
+                int randomX = random.Next(this.Size.Width - 100);
+                int randomY = random.Next(this.Size.Height - 100);
+                button.Location = new Point(randomX, randomY);
+
+                this.Controls.Add(button);
+
+
+            }
+        }
+
+       
+    }
+}
